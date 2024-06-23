c1d06c9897d5130ac4b849e411335f126f32257d
FacundoMag <facundo.maggeri.0608@gmail.com>
2024-06-23 11:42:52 -0300

    componentes creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
+import Registro from "./components/Registro";
+import IniciarSesion from "./components/IniciarSesion";
+import GestionPersonas from "./components/GestionPersonas";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class App extends Component {
+  state = {
+    token: null,
+    user: null,
+    mostrarRegistro: false
+  };
 
-  return (
-    <>
+  handleLogin = (token, user) => {
+    this.setState({ token, user });
+  };
+
+  handleLogout = () => {
+    this.setState({ token: null, user: null });
+  };
+
+  cambiarVista = () => {
+    this.setState({ mostrarRegistro: !this.state.mostrarRegistro });
+  };
+
+  render() {
+    const { token, mostrarRegistro } = this.state;
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {!token ? (
+          <div>
+            {mostrarRegistro ? (
+              <Registro cambiarVista={this.cambiarVista} />
+            ) : (
+              <IniciarSesion onLogin={this.handleLogin} cambiarVista={this.cambiarVista} />
+            )}
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.handleLogout}>Cerrar Sesión</button>
+            <GestionPersonas token={token} />
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
-
-export default App
