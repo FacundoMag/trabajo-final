38cd09b65a6efcb670c6e94e4a94b9ea150af101
FacundoMag <facundo.maggeri.0608@gmail.com>
2024-06-26 19:34:42 -0300

    cambios en los componentes Gestion y Lista

--- src/components/ListaPersonas.jsx
+++ src/components/ListaPersonas.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-const ListaPersonas = ({ personas, editarPersona, eliminarPersona }) => {
+const ListaPersonas = ({ personas, cargarPersonas }) => {
   return (
     <div>
       <h2>Listado de Personas</h2>
       <ul>
-        {personas.map(persona => (
+        {personas && personas.map(persona => (
           <li key={persona.persona_id}>
-            <p>{persona.nombres} {persona.apellidos}</p>
+            <p>Nombre: {persona.nombres} {persona.apellidos}</p>
             <p>Documento: {persona.documento}</p>
             <p>Fecha de Nacimiento: {persona.fechaNac}</p>
-            <p>Telefono: {persona.telefono}</p>
+            <p>Teléfono: {persona.telefono}</p>
             <p>Domicilio: {persona.domicilio}</p>
             <p>Email: {persona.mail}</p>
-            <button onClick={() => editarPersona(persona)}>Editar</button>
-            <button onClick={() => eliminarPersona(persona.persona_id)}>Eliminar</button>
           </li>
         ))}
       </ul>
+      <button onClick={() => cargarPersonas()}>Recargar Personas</button>
     </div>
   );
 };
